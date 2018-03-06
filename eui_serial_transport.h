ed08c86ffbbda7f86c8b23faae5d505f0aa235c5
Scott Rapson <scottapotamas@gmail.com>
2018-03-06 22:23:08 +1030

    Removed unused byte in the interface's msgID buffer
    We have strict msgID lengths now and shouldn't rely on null-terminated strings to handle this anyway.

--- eui_serial_transport.h
+++ eui_serial_transport.h
@@ -90,7 +90,7 @@ struct eui_interface {
     //buffer incoming data
     euiHeader_t inboundHeader;
     uint8_t header_data[2];
-    uint8_t inboundID[MESSAGEID_SIZE+1];
+    uint8_t inboundID[MESSAGEID_SIZE];
     uint16_t inboundOffset;
     uint8_t inboundData[PAYLOAD_SIZE_MAX];
     uint8_t crc_buffer;
