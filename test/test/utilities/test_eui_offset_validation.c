e3489b4f05e5b9e20bb603ed4529d7b8cf675ba4
Scott Rapson <scottapotamas@gmail.com>
2018-10-21 19:25:33 +1030

    Fixed bug where offset messages with sane inputs aren't passed through
    Adds tests.
    Found and patched issue where only invalid offset ranges -> fixed -> output was working.

--- test/test/utilities/test_eui_offset_validation.c
+++ test/test/utilities/test_eui_offset_validation.c
@@ -2,7 +2,7 @@
  
 // MODULE UNDER TEST
 #include "eui_offset_validation.h"
-
+#include "eui_config.h"
 // DEFINITIONS 
  
 // PRIVATE TYPES
@@ -27,32 +27,148 @@ void tearDown(void)
 
 void test_offset_validation_int8( void )
 {
-    TEST_IGNORE_MESSAGE("TODO: Test 1-byte input");
+    uint16_t output_start  = 0;
+    uint16_t output_end    = 0;
+
+    validate_offset_range(  0,
+                            4,
+                            TYPE_INT8,
+                            20,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 0, output_start );
+    TEST_ASSERT_EQUAL( 4, output_end );
+
+    validate_offset_range(  4,
+                            10,
+                            TYPE_INT8,
+                            20,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 4, output_start );
+    TEST_ASSERT_EQUAL( 10, output_end );
 }
 
 void test_offset_validation_int16( void )
 {
-    TEST_IGNORE_MESSAGE("TODO: Test 2-byte inputs");
+    uint16_t output_start  = 0;
+    uint16_t output_end    = 0;
+
+    validate_offset_range(  0,
+                            6,
+                            TYPE_INT16,
+                            40,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 0, output_start );
+    TEST_ASSERT_EQUAL( 6, output_end );
+
+    validate_offset_range(  6,
+                            14,
+                            TYPE_INT16,
+                            40,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 6, output_start );
+    TEST_ASSERT_EQUAL( 14, output_end );
 }
 
 void test_offset_validation_float( void )
 {
-    TEST_IGNORE_MESSAGE("TODO: Test 4-byte inputs");
+    uint16_t output_start  = 0;
+    uint16_t output_end    = 0;
+
+    validate_offset_range(  0,
+                            12,
+                            TYPE_FLOAT,
+                            40,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 0,  output_start );
+    TEST_ASSERT_EQUAL( 12, output_end );
+
+    validate_offset_range(  4,
+                            16,
+                            TYPE_FLOAT,
+                            40,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 4,  output_start );
+    TEST_ASSERT_EQUAL( 16, output_end );
 }
 
 void test_offset_validation_double( void )
 {
-    TEST_IGNORE_MESSAGE("TODO: Test 8-byte inputs");
+    uint16_t output_start  = 0;
+    uint16_t output_end    = 0;
+
+    validate_offset_range(  0,
+                            16,
+                            TYPE_DOUBLE,
+                            40,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 0,  output_start );
+    TEST_ASSERT_EQUAL( 16, output_end );
+
+    validate_offset_range(  8,
+                            24,
+                            TYPE_DOUBLE,
+                            40,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 8,  output_start );
+    TEST_ASSERT_EQUAL( 24, output_end );
 }
 
 void test_offset_validation_custom( void )
 {
-    TEST_IGNORE_MESSAGE("TODO: Test custom inputs");
+    uint16_t output_start  = 0;
+    uint16_t output_end    = 0;
+
+    validate_offset_range(  0,
+                            9,
+                            TYPE_CUSTOM,
+                            10,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 0,  output_start );
+    TEST_ASSERT_EQUAL( 9, output_end );
 }
 
 void test_offset_validation_swap_ends( void )
 {
-    TEST_IGNORE_MESSAGE("TODO: Test swapped start and end positions");
+    uint16_t output_start  = 0;
+    uint16_t output_end    = 0;
+
+    validate_offset_range(  4,
+                            0,
+                            TYPE_INT8,
+                            20,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 0, output_start );
+    TEST_ASSERT_EQUAL( 4, output_end );
+
+    validate_offset_range(  10,
+                            4,
+                            TYPE_INT8,
+                            20,
+                            &output_start,
+                            &output_end );
+
+    TEST_ASSERT_EQUAL( 4, output_start );
+    TEST_ASSERT_EQUAL( 10, output_end );
 }
 
 void test_offset_validation_overrun_input( void )
