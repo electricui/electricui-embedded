abdfcbc994a20cdad32030ab6fec366e4003ea0d
Scott Rapson <scottapotamas@gmail.com>
2018-10-29 18:13:18 +1030

    Added tests for packet parser userspace interface callbacks
    Tests the userspace callbacks from the parser, adds some error cases.

--- test/test/test_electricui_parse_packet.c
+++ test/test/test_electricui_parse_packet.c
@@ -3,6 +3,7 @@
 
 // MODULE UNDER TEST
 #include "electricui.h"
+#include "electricui_private.h"
 #include "mock_eui_serial_transport.h"
 #include "mock_eui_crc.h"
 #include "eui_offset_validation.h"
@@ -13,6 +14,9 @@ void stub_output_func( uint8_t byte );
 // PRIVATE TYPES
  
 // PRIVATE DATA
+uint8_t interface_cb_hit = 0;
+uint8_t interface_cb_arg[5] = { 0 };
+
 uint8_t test_cb_hit = 0;
 
 void test_cb( void )
@@ -44,9 +48,14 @@ void stub_output_func( uint8_t byte )
     //do nothing with it...
 }
 
-void stub_user_cb( uint8_t byte )
+void stub_user_cb( uint8_t flag )
 {
-    //do nothing with it...
+    if(interface_cb_hit < sizeof(interface_cb_arg))
+    {
+        interface_cb_arg[interface_cb_hit] = flag;
+    }
+
+    interface_cb_hit++;
 }
 
 
@@ -61,8 +70,13 @@ void setUp(void)
     test_pk_ptr = &test_interface.packet;
     setup_dev_msg(test_dev_msg, ARR_ELEM(test_dev_msg));
 
+    setup_interface( &test_interface, 1);
+
+
     test_uint = 20;
     test_cb_hit = 0;
+    interface_cb_hit = 0;
+    memset(&interface_cb_arg, 0, sizeof(interface_cb_arg));
 
 }
  
@@ -85,6 +99,8 @@ void test_ingest_unfinished( void )
     //expects a 'idle' code returned
     decode_packet_ExpectAndReturn( 0xAF, &test_interface.packet, 0);
     TEST_ASSERT_EQUAL_INT8( status_ok , parse_packet( 0xAF, &test_interface ) );
+    
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_hit );
 }
 
 // packet with a malformed crc, etc
@@ -106,6 +122,9 @@ void test_ingest_error( void )
 
     //expect the parse_packet to return an error to the user
     TEST_ASSERT_EQUAL_INT8( status_parser_generic , parse_packet( 0xAB, &test_interface ) );
+
+    TEST_ASSERT_EQUAL_INT8( 1, interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( cb_parse_failure, interface_cb_arg[0] );
 
     //we expect the handler to wipe the packet handler data after an error
     eui_packet_t blank_packet = { 0 };
@@ -135,6 +154,9 @@ void test_ingest_unknown_id( void )
 
     decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
     TEST_ASSERT_EQUAL_INT8( status_unknown_id , parse_packet( 0x00, &test_interface ) );
+
+    TEST_ASSERT_EQUAL_INT8( 1 , interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( cb_untracked , interface_cb_arg[0] );
 
     // handler should clean up after itself
     TEST_ASSERT_EQUAL( 0 , test_pk_ptr->parser.state);
@@ -167,6 +189,42 @@ void test_ingest_data_packet( void )
 
     //inbound byte is written to the local variable
     TEST_ASSERT_EQUAL_INT8(0x0A, test_uint);
+
+    TEST_ASSERT_EQUAL_INT8( 1 , interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( cb_tracked , interface_cb_arg[0] );
+
+    TEST_ASSERT_EQUAL( 0 , test_pk_ptr->parser.state);
+}
+
+// write a new value to the heartbeat internal byte
+void test_ingest_data_packet_internal( void )
+{
+    test_pk_ptr->parser.state           = exp_crc_b2;
+    test_pk_ptr->parser.id_bytes_in     = 1;
+    test_pk_ptr->parser.data_bytes_in   = 1;
+    test_pk_ptr->parser.frame_offset    = 0;
+
+    test_pk_ptr->header.data_len    = 1;
+    test_pk_ptr->header.type        = TYPE_UINT8;
+    test_pk_ptr->header.internal    = 1;
+    test_pk_ptr->header.offset      = 0;
+    test_pk_ptr->header.id_len      = 1;
+    test_pk_ptr->header.response    = 0;
+    test_pk_ptr->header.acknum      = 0;
+
+    memcpy(&test_pk_ptr->msgid_in, "h", 2);
+    test_pk_ptr->offset_in = 0;
+    test_pk_ptr->data_in[0] = 0x02;
+
+    test_pk_ptr->crc_in = 0xfefe;
+
+    decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
+    TEST_ASSERT_EQUAL_INT8( status_known_id , parse_packet( 0x00, &test_interface ) );
+
+    TEST_ASSERT_EQUAL_INT8( 2 , heartbeat );
+
+    TEST_ASSERT_EQUAL_INT8( 1 , interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( cb_tracked , interface_cb_arg[0] );
 
     TEST_ASSERT_EQUAL( 0 , test_pk_ptr->parser.state);
 }
@@ -227,13 +285,12 @@ void test_ingest_data_packet_exceeds_size( void )
     test_pk_ptr->crc_in = 0xfefe;
 
     decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
-    TEST_ASSERT_EQUAL_INT8( status_offset_er , parse_packet( 0x00, &test_interface ) );
-
+    TEST_ASSERT_EQUAL_INT8( 4 , parse_packet( 0x00, &test_interface ) );
+    //status_offset_er
     //inbound byte shouldn't be written to the local variable - check existing value intact
     TEST_ASSERT_EQUAL_INT8( 20, test_uint);
 
     TEST_ASSERT_EQUAL( 0 , test_pk_ptr->parser.state);
-
 }
 
 // offset packet with target range outside variable bounds
@@ -258,8 +315,8 @@ void test_ingest_data_packet_exceeds_range( void )
     test_pk_ptr->crc_in = 0xfefe;
 
     decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
-    TEST_ASSERT_EQUAL_INT8( status_offset_er , parse_packet( 0x00, &test_interface ) );
-
+    TEST_ASSERT_EQUAL_INT8( 4 , parse_packet( 0x00, &test_interface ) );
+    //status_offset_er
     //inbound byte shouldn't be written to the local variable - check existing value intact
     TEST_ASSERT_EQUAL_INT8( 20, test_uint);
 
@@ -424,7 +481,8 @@ void test_ingest_callback_packet_silent_invalid( void )
     test_pk_ptr->crc_in = 0xfefe;
 
     decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
-    TEST_ASSERT_EQUAL_INT8( status_missing_callback , parse_packet( 0x00, &test_interface ) );
+    TEST_ASSERT_EQUAL_INT8( 4 , parse_packet( 0x00, &test_interface ) );
+    //status_missing_callback
 
     TEST_ASSERT_EQUAL_INT8( 20, test_uint);
     TEST_ASSERT_EQUAL_INT8( 0, test_cb_hit);    //check our callback has been called
@@ -492,4 +550,146 @@ void test_ingest_callback_packet_query( void )
     TEST_ASSERT_EQUAL_INT8( 0, test_cb_hit);    //check our callback has been called
 
     TEST_ASSERT_EQUAL( 0 , test_pk_ptr->parser.state);
+}
+
+void test_ingest_interface_callback_handshake( void )
+{
+
+    test_pk_ptr->parser.state           = exp_crc_b2;
+    test_pk_ptr->parser.id_bytes_in     = 1;
+    test_pk_ptr->parser.data_bytes_in   = 0;
+
+    test_pk_ptr->header.data_len    = 0;
+    test_pk_ptr->header.type        = TYPE_CALLBACK;
+    test_pk_ptr->header.internal    = 1;
+    test_pk_ptr->header.id_len      = 1;
+    memcpy(&test_pk_ptr->msgid_in, EUI_INTERNAL_SEARCH, 1);
+
+    decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
+    encode_packet_simple_ExpectAnyArgsAndReturn(0);
+    encode_packet_simple_ExpectAnyArgsAndReturn(0);
+    encode_packet_simple_ExpectAnyArgsAndReturn(0);
+
+    parse_packet( 0x00, &test_interface );
+
+    //callbacks expected
+    TEST_ASSERT_EQUAL_INT8( 2, interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( cb_handshake, interface_cb_arg[0] );
+    TEST_ASSERT_EQUAL_INT8( cb_tracked, interface_cb_arg[1] );
+}
+
+// validate the developer interface space callbacks don't fire when not set
+void test_ingest_interface_callback_tracked_invalid( void )
+{
+    test_interface.interface_cb = 0;
+
+    test_pk_ptr->parser.state           = exp_crc_b2;
+    test_pk_ptr->parser.id_bytes_in     = 4;
+    test_pk_ptr->parser.data_bytes_in   = 1;
+
+    test_pk_ptr->header.data_len    = 1;
+    test_pk_ptr->header.type        = TYPE_UINT8;
+    test_pk_ptr->header.id_len      = 4;
+    memcpy(&test_pk_ptr->msgid_in, "data", 4);
+
+    decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
+    parse_packet( 0x00, &test_interface );
+
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_arg[0] );
+}
+
+void test_ingest_interface_callback_untracked_invalid( void )
+{
+    test_interface.interface_cb = 0;
+
+    test_pk_ptr->parser.state           = exp_crc_b2;
+    test_pk_ptr->parser.id_bytes_in     = 4;
+    test_pk_ptr->parser.data_bytes_in   = 1;
+
+    test_pk_ptr->header.data_len    = 1;
+    test_pk_ptr->header.type        = TYPE_UINT8;
+    test_pk_ptr->header.id_len      = 4;
+    memcpy(&test_pk_ptr->msgid_in, "rand", 4);
+
+    decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
+    parse_packet( 0x00, &test_interface );
+
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_arg[0] );
+}
+
+void test_ingest_interface_callback_error_invalid( void )
+{
+    test_interface.interface_cb = 0;
+
+    test_pk_ptr->parser.state           = exp_data;
+    test_pk_ptr->parser.id_bytes_in     = 0;
+    test_pk_ptr->parser.data_bytes_in   = 0;
+    test_pk_ptr->parser.frame_offset    = 0;
+
+    test_pk_ptr->header.data_len    = 6;
+    test_pk_ptr->header.type        = TYPE_UINT8;
+    test_pk_ptr->header.internal    = 0;
+    test_pk_ptr->header.id_len      = 3;
+
+    decode_packet_ExpectAndReturn( 0xAB, &test_interface.packet, parser_error);
+    parse_packet( 0xAB, &test_interface );
+
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_arg[0] );
+}
+
+void test_ingest_interface_callback_handshake_no_ptr( void )
+{
+    //no dev callback, but the interface is still live
+    test_interface.interface_cb = 0;
+
+    test_pk_ptr->parser.state           = exp_crc_b2;
+    test_pk_ptr->parser.id_bytes_in     = 1;
+    test_pk_ptr->parser.data_bytes_in   = 0;
+
+    test_pk_ptr->header.internal    = 1;
+    test_pk_ptr->header.data_len    = 0;
+    test_pk_ptr->header.type        = TYPE_CALLBACK;
+    test_pk_ptr->header.id_len      = 1;
+    memcpy(&test_pk_ptr->msgid_in, EUI_INTERNAL_SEARCH, 1);
+
+    decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
+    encode_packet_simple_ExpectAnyArgsAndReturn(0);
+    encode_packet_simple_ExpectAnyArgsAndReturn(0);
+    encode_packet_simple_ExpectAnyArgsAndReturn(0);
+
+    parse_packet( 0x00, &test_interface );
+
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_arg[0] );
+}
+
+void test_ingest_interface_callback_handshake_no_interface( void )
+{
+    // with no configured interface, the print output function won't operate
+    // this test is included because previously had segfaults when the interface wasn't
+    // setup prior to testing a null user callbacks
+    
+    setup_interface( 0, 0); //nulls out the setup in the TEST_SETUP()
+    test_interface.interface_cb = 0;
+
+    test_pk_ptr->parser.state           = exp_crc_b2;
+    test_pk_ptr->parser.id_bytes_in     = 1;
+    test_pk_ptr->parser.data_bytes_in   = 0;
+
+    test_pk_ptr->header.internal    = 1;
+    test_pk_ptr->header.data_len    = 0;
+    test_pk_ptr->header.type        = TYPE_CALLBACK;
+    test_pk_ptr->header.id_len      = 1;
+    memcpy(&test_pk_ptr->msgid_in, EUI_INTERNAL_SEARCH, 1);
+
+    decode_packet_ExpectAndReturn( 0x00, &test_interface.packet, parser_complete);
+    //no output function means no printed handshake calls
+    parse_packet( 0x00, &test_interface );
+
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_hit );
+    TEST_ASSERT_EQUAL_INT8( 0, interface_cb_arg[0] );
+
 }
