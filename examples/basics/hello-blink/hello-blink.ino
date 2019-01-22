5a3983fc22fc099ad3255c47725e5cbd3780a802
Scott Rapson <scottapotamas@gmail.com>
2019-01-22 22:43:31 +1030

    Cleanup hello_blink example sketch
    Blinks LED by default.
    Fixes led blinking overflow. Closes #110.
    Formatting and comments reworked.

--- examples/basics/hello-blink/hello-blink.ino
+++ examples/basics/hello-blink/hello-blink.ino
@@ -4,44 +4,48 @@
 #define LED_PIN 13
 
 // Simple variables to modify the LED behaviour
-uint8_t   blink_enable = 0; //if the blinker should be running
+uint8_t   blink_enable = 1; //if the blinker should be running
 uint8_t   led_state  = 0;   //track if the LED is illuminated
 uint16_t  glow_time  = 200; //in milliseconds
 uint16_t  dark_time  = 200; //in milliseconds
 
-// Keep track of how much time we've spent on or off
-uint16_t time_on = 0;
-uint16_t time_off = 0;
+// Keep track of when the light turns on or off
+uint32_t time_on = 0;
+uint32_t time_off = 0;
 
-// ElectricUI tracks variables referenced in this array
+// Electric UI manages variables referenced in this array
 eui_message_t dev_msg_store[] = 
 {
-    EUI_UINT8(  "led_blink",  blink_enable ),
-    EUI_UINT8(  "led_state",  led_state ),
-    EUI_UINT16( "lit_time",   glow_time ),
-    EUI_UINT16( "unlit_time", dark_time ),
+  EUI_UINT8(  "led_blink",  blink_enable ),
+  EUI_UINT8(  "led_state",  led_state ),
+  EUI_UINT16( "lit_time",   glow_time ),
+  EUI_UINT16( "unlit_time", dark_time ),
 };
 
-// Instantiate the communication interface
+// Instantiate the communication interface's management object
 eui_interface_t serial_comms; 
 
 void setup() 
 {
-  Serial.begin(115200);   // Microcontroller's primary serial interface (usually the USB cable)
+  // Setup the serial port and status LED
+  Serial.begin( 115200 );
+  pinMode( LED_PIN, OUTPUT );
 
-  // Setup the output interface
+  // Each communications interface uses a settable output function
   serial_comms.output_func = &tx_putc;
-  setup_interface(&serial_comms);
+
+  // Provide the library with the interface we just setup
+  setup_interface( &serial_comms );
 
   // Provide the tracked variables to the library
-  EUI_TRACK(dev_msg_store);
+  EUI_TRACK( dev_msg_store );
 
-  // Provide a identifier to make it easy to find in the UI
-  setup_identifier("hello", 5);
+  // Provide a identifier to make this board easy to find in the UI
+  setup_identifier( "hello", 5 );
+
 
   time_on = millis();
   time_off = millis();
-
 }
 
 void loop() 
@@ -50,41 +54,44 @@ void loop()
 
   if( led_state == LOW ) //LED is off
   {
-      if( millis() - time_off > dark_time )
-      {
-          led_state = 1;
-          time_on = millis();
-      }
+    // Check if the LED has been off for the configured duration
+    if( millis() - time_off >= dark_time )
+    {
+      led_state = HIGH;
+      time_on = millis();
+    }
   }
-  else  //led is on
+  else  //LED is on
   {
-      if( millis() - time_on > glow_time )
-      {
-          led_state = 0;
-          time_off = millis();
-      }
+    if( millis() - time_on >= glow_time )
+    {
+      led_state = LOW;
+      time_off = millis();
+    }
   }
 
-  if(blink_enable == 0)
+  if( blink_enable )
   {
-      digitalWrite( LED_PIN, LOW );
+    digitalWrite( LED_PIN, led_state ); //allow the led to blink
   }
   else
   {
-      digitalWrite( LED_PIN, led_state ); //allow the led to blink
+    digitalWrite( LED_PIN, led_state );
   }
 
 }
 
 void uart_rx_handler()
 {
-    while(Serial.available() > 0)  // While we have data, we will pass those bytes to the ElectricUI parser
-    {  
-        parse_packet(Serial.read(), &serial_comms);  // Ingest a byte
-    }
+  // While we have data, we will pass those bytes to the ElectricUI parser
+  while( Serial.available() > 0 )  
+  {  
+    parse_packet( Serial.read(), &serial_comms );  // Ingest a byte
+  }
 }
-
-void tx_putc(uint8_t data)
+  
+void tx_putc( uint8_t data )
 {
-    Serial.write(data);  //output on the main serial port
+  //output on the main serial port
+  Serial.write( data );
 }
