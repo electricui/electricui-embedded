3c0e819465d2da0d2e49577b33b987945cfb3101
Scott Rapson <scottapotamas@gmail.com>
2019-02-06 11:32:52 +1030

    Fixed stale fuzzing test program
    Allows fuzzing to occur with current changes.
    Adds some additional types to the tracked variables.

--- test/fuzz/afl-eui.c
+++ test/fuzz/afl-eui.c
@@ -8,15 +8,22 @@
 #include "../../src/electricui.h"
 
 uint8_t   test_byte  = 10;
-uint16_t  test_2byte  = 200;
+uint16_t  test_2byte = 300;
+uint32_t  test_4byte = 1234567890;
+float     test_float = 3.1415927f;
+double    test_double = 3.1415926535897931;
 
 eui_message_t afl_msg_store[] = 
 {
     EUI_UINT8(  "u8",  test_byte ),
     EUI_UINT16( "u16", test_2byte ),
+    EUI_UINT32( "u32", test_4byte ),
+    EUI_FLOAT(  "fp",  test_float ),
+    EUI_DOUBLE( "dfp", test_double ),
+
 };
 
-void afl_putc( uint8_t data );
+void afl_putc( uint8_t *data, uint16_t len );
 
 int main() 
 {
@@ -25,7 +32,7 @@ int main()
     // eUI Setup
     eui_interface_t afl_comms; 
     afl_comms.output_func = &afl_putc;
-    setup_interface( &afl_comms, 1 );
+    setup_interface( &afl_comms );
     EUI_TRACK( afl_msg_store );
     setup_identifier( "fuzzy", 5 );
 
@@ -49,7 +56,9 @@ int main()
     return 0;
 }
 
-void afl_putc( uint8_t data )
+void afl_putc( uint8_t *data, uint16_t len )
 {
-    //we don't care about output
+    //we don't care about output, but print it as hex
+    data[len] = 0;  //make sure the last byte is 0, then print as string
+    printf("%s",(char*)data);
 }
