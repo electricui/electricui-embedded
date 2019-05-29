d745972804f30dd76b67e71e634482dacfd9a288
Scott <scottapotamas@gmail.com>
2019-05-29 13:27:51 +0930

    Rework arduino sketch checker to blacklist incompatible examples
    Skips files based on blacklist so ESP32 or bluetooth examples don't get attempted (then failed) on a Uno etc.
    Also added colouring to OK/SKIP/FAIL output text, and formatting improvements.

--- test/arduino-example-test.sh
+++ test/arduino-example-test.sh
@@ -10,48 +10,87 @@ user_hidden_path=~/.arduino15
 hardware_path=$bin_path/hardware
 tool_arg=$bin_path/tools-builder
 toolchain=arduino-builder
-declare -a target_platform=(	"arduino:avr:leonardo" 
-								"arduino:avr:uno" 
-								"arduino:avr:mega:cpu=atmega2560"
-								"esp32:esp32:esp32thing:FlashFreq=80,UploadSpeed=921600"
-							)
 
-# todo test the esp32 and 8266 as it uses g++ toolchain!
+# Platforms are defined with the platform control string expected by arduino-builder
+# and then following arguments are sketches which should NOT be tested (blacklist)
+# space-delimiting is used.
+# Add new platforms by appending new target_platform array indices
+declare -a target_platforms
+target_platforms[0]="arduino:avr:leonardo esp32-ble.ino esp32-websockets.ino bluefruit-bleuart.ino"
+target_platforms[1]="arduino:avr:uno esp32-ble.ino esp32-websockets.ino bluefruit-bleuart.ino" 
+target_platforms[2]="arduino:avr:mega:cpu=atmega2560 esp32-ble.ino esp32-websockets.ino bluefruit-bleuart.ino"
+target_platforms[3]="esp32:esp32:esp32thing:FlashFreq=80,UploadSpeed=921600 persistence-eeprom.ino bluefruit-bleuart.ino"
 
 # track test progress
 tests_run=0
 tests_pass=0
 tests_fail=0
 
-## Run tests for each platform
-for platform in "${target_platform[@]}"
+# Bash escape codes for coloured output
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+LRED='\033[1;31m'
+BOLD='\033[1m'
+NF='\033[0m' # No Color
+
+for platform in "${target_platforms[@]}"
 do
-	echo "Checking examples for $platform"
+    # Deconstruct the target_platforms array into elements based on delimiter
+    IFS=" " read -r -a arr <<< "${platform}"
+
+	echo -e "Checking examples for ${BOLD}${arr[0]}${NF}"
+
+	# Iterate sketch files in the examples directory
 	for sketch in `find ../examples/ -name '*.ino'`
 	do
-		tests_run=$((tests_run+1))
-		# Run the build against the platform, capture (and suppress) output
-		result=`$bin_path/$toolchain                       \
-		-hardware $hardware_path                           \
-		-hardware $user_hidden_path/packages               \
-		-tools $tool_arg                                   \
-		-tools $hardware_path/tools/avr                    \
-		-tools $user_hidden_path/packages                  \
-		-libraries $user_sketchbook/libraries              \
-		-libraries ../src                                  \
-		-fqbn $platform $sketch 2>&1`
+		# Check if the filename is in the blacklist
+		sketch_name=$(basename "$sketch")	#just the filename, not whole path
+		sketch_valid=0
 
-		# Check the build pass/fail status
-		if [ $? -eq 0 ]
+		for blacklisted in "${arr[@]}"
+		do
+			if [[ $blacklisted == $sketch_name ]];
+			then
+				sketch_valid=1
+			fi
+		done 
+
+		# Try compiling the file if its not blacklisted
+		if [[ $sketch_valid == 0 ]];
 		then
-			echo "$(basename -a -s .ext $sketch) OK"
-			tests_pass=$((tests_pass+1))
+			tests_run=$((tests_run+1))
+			# Run the build against the platform, capture (and suppress) output
+			result=`$bin_path/$toolchain                       \
+			-hardware $hardware_path                           \
+			-hardware $user_hidden_path/packages               \
+			-tools $tool_arg                                   \
+			-tools $hardware_path/tools/avr                    \
+			-tools $user_hidden_path/packages                  \
+			-libraries $user_sketchbook/libraries              \
+			-libraries ../src                                  \
+			-fqbn ${arr[0]} $sketch 2>&1`
+
+			# Check the build pass/fail status
+			if [ $? -eq 0 ]
+			then
+				echo -e "   $(basename -a -s .ext $sketch) ${GREEN}OK${NF}"
+				tests_pass=$((tests_pass+1))
+			else
+				echo -e "   $(basename -a -s .ext $sketch) ${RED}FAIL${NF}" >&2
+				tests_fail=$((tests_fail+1))
+				echo
+				echo -e "Output: ${LRED}$result${NF}"
+				echo
+			fi
 		else
-			echo "$(basename -a -s .ext $sketch) FAIL" >&2
-			tests_fail=$((tests_fail+1))
-			echo "Output: $result"
+			echo -e "   $(basename -a -s .ext $sketch) ${YELLOW}SKIP${NF}"
 		fi
+
 	done
+
+	echo
+
 done
 
 echo "Result:  RAN:$tests_run | PASS:$tests_pass | FAIL:$tests_fail"
