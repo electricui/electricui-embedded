69a9b5ac4dbca92f7ca7208ff03cf9d5d2651650
Scott Rapson <scottapotamas@gmail.com>
2020-12-21 00:55:15 +1030

    Added heartbeat tracking to status-error-callback example

--- examples/basics/status-error-callbacks/status-error-callbacks.ino
+++ examples/basics/status-error-callbacks/status-error-callbacks.ino
@@ -1,12 +1,13 @@
 /*
  *	Error handling and status returns are provided by the parser function,
- *	catching and handling these error cases is typically not required, but can aid debugging
- *  or allow for custom functionality to sit onto the existing protocol and library.
+ *	handling these error cases is typically not required, but can aid debugging
+ *  or allow for added functionality to the existing library.
  *  
  *  Interface callbacks can be used to handle untracked messages, flag an event while 
  *  tracked data is processed, or inspect the inbound buffer when an error was flagged.
  *
  *  report_error( ... ) demonstrates runtime variable creation for sending arbitrary strings.
+ *  Heartbeat tracking demo code is shown inside the callback's EUI_CB_TRACKED section.
 */
 
 #include "electricui.h"
@@ -17,15 +18,20 @@ uint8_t   led_state    = 0;
 uint16_t  glow_time    = 200;
 uint32_t  led_timer    = 0;
 
-char device_name[] = "hello-errors";
+char device_name[] = "hello-callback";
+
+#define HEARTBEAT_EXPECTED_MS 800
+uint32_t last_heartbeat = 0;
+uint8_t heartbeat_ok_count = 0;
+
+uint32_t heartbeat_dt = 0;
 
 // Use the EUI_INTERFACE_CB macro to include the diagnostics callback 
 eui_interface_t serial_interface = EUI_INTERFACE_CB( &serial_write, &eui_callback ); 
 
 eui_message_t tracked_variables[] = 
 {
-  EUI_UINT8(  "led_blink",  blink_enable ),
-  EUI_UINT8(  "led_state",  led_state ),
+  EUI_UINT16( "led_state",  led_state ),
   EUI_UINT16( "lit_time",   glow_time ),
   EUI_CHAR_ARRAY( "name",   device_name ),
 };
@@ -46,17 +52,29 @@ void setup()
 void loop() 
 {
   serial_rx_handler();
+  is_connection_ok();
 
-  if( blink_enable )
+  if( heartbeat_dt < 256 )
   {
-    if( millis() - led_timer >= glow_time )
-    {
-      led_state = !led_state;
-      led_timer = millis();
-    }    
+    led_state = 255 - heartbeat_dt;
+    analogWrite( LED_BUILTIN, led_state );
   }
 
-  digitalWrite( LED_BUILTIN, led_state );
+}
+
+bool is_connection_ok( void )
+{
+  heartbeat_dt = millis() - last_heartbeat;
+
+  // Check if the most recent heartbeat has timed out
+  if( heartbeat_dt > HEARTBEAT_EXPECTED_MS )
+  {
+      heartbeat_ok_count = 0;
+  }
+  
+  // Connection is considered OK if more than 3 heartbeats have arrived within 
+  // the threshold duration
+  return ( heartbeat_ok_count > 3 );
 }
 
 void serial_rx_handler()
@@ -71,7 +89,7 @@ void serial_rx_handler()
     switch( parse_status.parser )
     {
       case EUI_PARSER_IDLE:
-      // Waiting for more data to complete a valid message
+        // Waiting for more data to complete a valid message
       break;
 
       case EUI_PARSER_OK:
@@ -79,39 +97,37 @@ void serial_rx_handler()
       break;
 
       case EUI_PARSER_ERROR:
-      // The parser encountered an error or failure, e.g. CRC check fails, invalid packet formats
-      report_error("Parser Fail");
+        // The parser encountered an error or failure, e.g. CRC check fails, invalid packet formats
+        report_error("Parser Fail");
       break;
     }
 
     switch( parse_status.action )
     {
       case EUI_ACTION_CALLBACK_ERROR:
-      // Couldn't call a developer tracked callback function due to null pointer reference
-      report_error("Callback Fail");
+        // Couldn't call a developer tracked callback function due to null pointer reference
+        report_error("Callback Fail");
       break;
 
       case EUI_ACTION_WRITE_ERROR:
       {
-      // Inbound packet has invalid offset data or invalid length which would result in 
-      // out-of-bounds data operations. When this occurs, data is not written
-      eui_header_t header   = serial_interface.packet.header;
-      uint8_t      *name_rx = serial_interface.packet.id_in;
-      eui_message_t *tracked = find_tracked_object( (char *)name_rx );
-      uint8_t size_expected = tracked->size;
+        // Inbound packet has invalid offset data or invalid length which would result in 
+        // out-of-bounds data operations. When this occurs, data is not written
+        eui_header_t header   = serial_interface.packet.header;
+        uint8_t      *name_rx = serial_interface.packet.id_in;
+        eui_message_t *tracked = find_tracked_object( (char *)name_rx );
+        uint8_t size_expected = tracked->size;
 
+        char error_buf[50] = { 0 };
+        snprintf(error_buf, 50, "Write Err: got %d, exp %d", header.data_len, size_expected );
 
-      char error_buf[50] = { 0 };
-      snprintf(error_buf, 50, "Write Err: got %d, exp %d", header.data_len, size_expected );
-
-      report_error( error_buf );
-
+        report_error( error_buf );
       }
       break;
 
       case EUI_ACTION_TYPE_MISMATCH_ERROR:
-      // Data type in the inbound packet doesn't match the internal type
-      report_error("Type Fail");
+        // Data type in the inbound packet doesn't match the internal type
+        report_error("Type Fail");
       break;
     }
 
@@ -147,16 +163,32 @@ void report_error( char * error_string )
                               { .data = error_string }       };
 
   eui_send_untracked( &err_message );
-
 }
 
+// The library calls this callback after valid packet processing
 void eui_callback( uint8_t message )
 {
   switch(message)
   {
     case EUI_CB_TRACKED:
+    {
       // UI received a tracked message ID and has completed processing
 
+      // Watch for the internal heartbeat message to come in
+      uint8_t *name_rx = serial_interface.packet.id_in;
+
+      if( strcmp( (char*)name_rx, EUI_INTERNAL_HEARTBEAT ) == 0 )
+      {
+        if( millis() - last_heartbeat < HEARTBEAT_EXPECTED_MS )
+        {
+          // Increment the counter, but clamp to 20
+          heartbeat_ok_count = min(heartbeat_ok_count++, 20);
+        }
+
+        last_heartbeat = millis();
+      }
+
+    }
     break;
 
     case EUI_CB_UNTRACKED:
