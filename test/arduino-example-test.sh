d70e965c28cdc4fee1fc2bda78afe03df95715e6
Scott Rapson <scottapotamas@gmail.com>
2021-10-11 22:42:52 +1030

    Resolve CI arduino-test failing to download ESP32 board package

--- test/arduino-example-test.sh
+++ test/arduino-example-test.sh
@@ -6,10 +6,10 @@
 
 
 # Update the arduino-cli index and install deps
-arduino-cli core update-index --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_dev_index.json
+arduino-cli core update-index
 arduino-cli core install arduino:avr
 arduino-cli core install arduino:samd
-arduino-cli core install esp32:esp32 
+arduino-cli core install esp32:esp32 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_dev_index.json
 
 # Websockets library is a dep for the ESP32 websockets example
 ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true arduino-cli lib install --git-url https://github.com/Links2004/arduinoWebSockets
