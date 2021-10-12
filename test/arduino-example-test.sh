2e9a369a290d2db0e1d17575dcea2bf58ac182c7
Scott Rapson <scottapotamas@gmail.com>
2021-10-12 11:27:38 +1030

    Fix arduino-cli failing to find ESP32 board package during install

--- test/arduino-example-test.sh
+++ test/arduino-example-test.sh
@@ -6,7 +6,7 @@
 
 
 # Update the arduino-cli index and install deps
-arduino-cli core update-index
+arduino-cli core update-index --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_dev_index.json
 arduino-cli core install arduino:avr
 arduino-cli core install arduino:samd
 arduino-cli core install esp32:esp32 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_dev_index.json
